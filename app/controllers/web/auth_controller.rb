58a2a1f5ff5754d8fdc519dc763638adf9ab0a9d
Stanislav Kolkov <s.kolkov@3commas.io>
2023-01-15 09:29:18 +0300

    logs

--- app/controllers/web/auth_controller.rb
+++ app/controllers/web/auth_controller.rb
@@ -7,10 +7,12 @@ module Web
       user_info = request.env['omniauth.auth']
       # raise user_info # Your own session management should be placed here.
       # binding.irb
+      p 'req'
     end
 
     def callback
       # binding.irb
+      p 'callback'
     end
   end
 end

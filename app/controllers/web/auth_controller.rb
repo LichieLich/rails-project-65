431a536c5389ea37e55ee41b3dc682a3d8828b9c
Stanislav Kolkov <s.kolkov@3commas.io>
2023-01-19 23:17:13 +0300

    Correct routes + request in callback

--- app/controllers/web/auth_controller.rb
+++ app/controllers/web/auth_controller.rb
@@ -3,16 +3,10 @@
 module Web
   class AuthController < Web::ApplicationController
     def request
-      binding.irb
-      user_info = request.env['omniauth.auth']
-      # raise user_info # Your own session management should be placed here.
-      # binding.irb
-      raise 'req'
     end
 
     def callback
-      # binding.irb
-      raise 'callback'
+      user_info = request.env['omniauth.auth']
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@
 
 Rails.application.routes.draw do
   scope module: :web do
-    post 'auth/github', to: 'auth#request', as: :auth_request
-    get 'auth/github/callback', to: 'auth#callback', as: :callback_auth
+    post 'auth/:provider', to: 'auth#request', as: :auth_request
+    get 'auth/:provider/callback', to: 'auth#callback', as: :callback_auth
 
     root 'home#index'
   end

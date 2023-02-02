9d9748b53d0505040860e8ee9934dca89294eaf7
Stanislav Kolkov <s.kolkov@3commas.io>
2023-02-02 13:13:59 +0300

    Added omniauth logger

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,4 +2,5 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, ENV.fetch('GITHUB_CLIENT_ID', nil), ENV.fetch('GITHUB_CLIENT_SECRET', nil), scope: 'user'
+  OmniAuth.config.logger = Rails.logger
 end

0d1784f87b0525b6cacc340eb6b2141155ae2ffc
Stanislav Kolkov <s.kolkov@3commas.io>
2023-03-02 22:48:46 +0300

    Changed category name type to string + view fix

---
+++ db/migrate/20230302194527_change_category_name_to_string.rb
@@ -0,0 +1,5 @@
+class ChangeCategoryNameToString < ActiveRecord::Migration[7.0]
+  def change
+    change_column :categories, :name, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_09_154250) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_02_194527) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -52,7 +52,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_09_154250) do
   end
 
   create_table "categories", force: :cascade do |t|
-    t.text "name"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
